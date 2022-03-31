5c64360cfd237aaad3c6b5d0d2e21ba01db9101b
Justin Benedick <beanisgood@gmail.com>
2022-03-30 20:52:54 -0500

    updated sample scripts
    updated sample scripts

--- scripts/run-Q3-instagib-1v1.sh
+++ scripts/run-Q3-instagib-1v1.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "Edit this script to change the path to ioquake3's dedicated server executable and which binary if you aren't on x86_64.\n Set the sv_dlURL setting to a url like http://yoursite.com/ioquake3_path for ioquake3 clients to download extra data"
-~/ioquake3/ioq3ded.x86_64 +set fs_game InstaUnlagged +exec DefaultServer-Tourney.cfg
+~/ioquake3/ioq3ded.x86_64 +set fs_game InstaUnlagged +exec DefaultServer-Tourney.cfg +exec playlists.cfg +vstr stock-1v1-map1
  
--- scripts/run-Q3-instagib-ctf.sh
+++ scripts/run-Q3-instagib-ctf.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 echo "Edit this script to change the path to ioquake3's dedicated server executable and which binary if you aren't on x86_64.\n Set the sv_dlURL setting to a url like http://yoursite.com/ioquake3_path for ioquake3 clients to download extra data"
-~/ioquake3/ioq3ded.x86_64 +set fs_game InstaUnlagged +exec DefaultServer-CTF.cfg 
+~/ioquake3/ioq3ded.x86_64 +set fs_game InstaUnlagged +exec DefaultServer-CTF.cfg +exec playlist.cfg +vstr stock-ctf-1
+
+//currently broken
